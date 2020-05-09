3b7e2e245c47097d826f06dee47510b2a794504e
Anette Rivera <anettem.rivera@gmail.com>
2020-05-09 00:31:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:grace_hopper]
+     
 
 
 end
@@ -37,6 +40,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
+   
 end
 
 def dennis_ritchies_language
@@ -55,6 +61,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     
+     return programmer_hash[:dennis_ritchie][:languages].join
+    
 end
 
 def adding_matz
@@ -80,7 +90,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash { :yukihiro_matsumoto}
 end
 
 def changing_alan
